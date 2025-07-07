0c60cbabe593a57740c872df115edab17054cb1f
Renaanprofissional <renaan.profissional@gmail.com>
2025-07-07 19:39:13 +0000

    New Version

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 /*
-[] Descobrir quando o botão foi clicado.
-[] Pegar o que foi escrito no input.
+[x] Descobrir quando o botão foi clicado.
+[x] Pegar o que foi escrito no input.
 [] Enviar para o N8N.
 [] Receber o que o N8N Respondeu.
 [] Colocar na tela o que ele respondeu.
 */
+
+function cliqueiNoBotao(){
+    let textoInput = document.querySelector('.input-animacao').value
+}
