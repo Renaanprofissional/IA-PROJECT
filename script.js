839dccbdd686c46ee30b940d229c0a754b5604b7
Renaanprofissional <renaan.profissional@gmail.com>
2025-07-07 19:39:44 +0000

    New version

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*
 [x] Descobrir quando o botão foi clicado.
-[x] Pegar o que foi escrito no input.
+[] Pegar o que foi escrito no input.
 [] Enviar para o N8N.
 [] Receber o que o N8N Respondeu.
 [] Colocar na tela o que ele respondeu.
